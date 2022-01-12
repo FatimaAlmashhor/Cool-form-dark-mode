ba0997a83ae10a2ce4cbc4237fa878a293968ba7
FatimaAlmashhor <fatima.amin4official@gmail.com>
2022-01-12 15:39:10 +0300

    Fix : delete unnassary code

--- js/darkMode.js
+++ js/darkMode.js
@@ -2,7 +2,7 @@ const checkbox = document.getElementById('checkbox');
 
 checkbox.addEventListener('change', () => {
     document.body.classList.toggle('dark');
-    let value = getComputedStyle(document.documentElement)
-        .setProperty('--text-color', '#fff');
-    console.log(value);
+    // let value = getComputedStyle(document.documentElement)
+    //     .setProperty('--text-color', '#fff');
+    // console.log(value);
 })
